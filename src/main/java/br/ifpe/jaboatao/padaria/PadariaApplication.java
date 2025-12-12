def488dd0799af2dd4801dce1e64b610f059235d
MiguelSoares23 <>
2025-12-12 10:43:51 -0300

    ...

--- src/main/java/br/ifpe/jaboatao/padaria/PadariaApplication.java
+++ src/main/java/br/ifpe/jaboatao/padaria/PadariaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PadariaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PadariaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PadariaApplication.class, args);
+    }
 
 }
